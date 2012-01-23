5229f095fa48c9422cc5abac8eb572cca34c31c2
adixime <adixime@gmail.com>
2012-01-23 11:26:59 -0500

    we are starting now at 11:26 am

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Contact form</title>
+    <link href="css/general.css" rel="styleshet">
+</head>
+
+	<form methods="post" action="index.php">
+    	<div>
+        	<label for="name">Name</label>
+            <input id="name" name="name">
+        </div>
+        <div>
+        	<label for="email">E-mail Address</label>
+            <input type="email" id="email" name="email">
+        </div>
+        <div>
+        	<label for="message">Message</label>
+            <textarea id="message" name="message"></textarea>
+        </div>
+        <div>
+        	<button type="submit">Send Message</button>
+        </div>
+
+
+
+
+<body>
+</body>
+</html>
